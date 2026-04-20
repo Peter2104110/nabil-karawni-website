36e4747e287b46ac18255c84027af6a8d8ccdafd
Peter2104110 <peterkirawni1@gmail.com>
2026-04-20 18:36:55 +0300

    fix: analytics injection

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,4 +2,13 @@ import { createRoot } from "react-dom/client";
 import App from "./App";
 import "./index.css";
 
+const endpoint = import.meta.env.VITE_ANALYTICS_ENDPOINT;
+
+if (endpoint) {
+  const script = document.createElement("script");
+  script.src = `${endpoint}/umami`;
+  script.defer = true;
+  document.head.appendChild(script);
+}
+
 createRoot(document.getElementById("root")!).render(<App />);
